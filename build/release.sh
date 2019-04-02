5616229176369a322195ba17d58ae2e6c074222a
Jialiang T <rememhe@outlook.com>
2019-04-02 11:13:04 +0800

    build: add release script

---
+++ build/release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+git checkout master
+
+git merge dev
+
+echo "Enter release version: "
+
+read VERSION
+
+VERSION=`npm version $VERSION --no-git-tag-version`
+
+echo "release version: "$VERSION
+
+git add .
+
+git commit -m "release: $VERSION"
+
+git tag "$VERSION"
+
+git push
+
+git push origin --tags
+
+git checkout dev
+
+git rebase master
+
+git push
